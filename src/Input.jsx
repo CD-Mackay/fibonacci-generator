7dd3a2b9c1efcfb0d030e0b0bddf981979aadfa4
CD-Mackay <connor.mackay@gmail.com>
2021-03-08 11:56:14 -0500

    create input form

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Input(props) {
-  
+  const [numone, setNumone] = useState(0);
+  const [numTwo, setNumTwo] = useState(0);
+  const [sequence, setSequence] = useState([]);
   function create () {
     props.onSubmit(1, 2)
   }
