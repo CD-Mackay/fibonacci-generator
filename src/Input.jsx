ec95410ba71e03670118e782f4c6b6e80cfe9344
CD-Mackay <connor.mackay@gmail.com>
2021-03-09 13:50:35 -0500

    style rendering

--- src/Input.jsx
+++ src/Input.jsx
@@ -5,20 +5,31 @@ import Output from './Output';
 export default function Input(props) {
   const [numone, setNumone] = useState(0);
   const [numTwo, setNumTwo] = useState(0);
+  const [numThree, setNumThree] = useState(0);
   const [sequence, setSequence] = useState([]);
   function create () {
-  const fib = props.onSubmit(numone, numTwo);
+  const fib = props.onSubmit(numone, numTwo, numThree);
   setSequence([fib]);
   }
 
   return (
     <div>
-    <form onSubmit={event => event.preventDefault()} >
+    <form onSubmit={event => event.preventDefault()} id="input-form">
+      <div class="seperator">
+      <label for="numOne">Enter the first number</label>
       <input type="number" onChange={e => setNumone(e.target.value)} />
+      </div>
+      <div class="seperator">
+      <label for="numTwo">Enter the second number</label>
       <input type="number" onChange={e => setNumTwo(e.target.value)} />
+      </div>
+      <div class="seperator">
+        <label for="numentries">How entries should be run?</label>
+        <input type="number" onChange={e => setNumThree(e.target.value)}></input>
+      </div>
       <button onClick={create} >Submit!</button>
     </form>
-    <Output sequence={sequence} />
+    {sequence.length > 0 && <Output sequence={sequence} />}
      </div>
   )
 }
--- src/Output.jsx
+++ src/Output.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import './styles.scss';
 
 export default function Output(props) {
+  
   const renderSequence = props.sequence[0].map(n => {
-    return <li>{n}</li>
+    return <li key={n}>{n}</li>
   })
   return (
+    <div id="output">
     <ul>
     { renderSequence }
     </ul>
+    </div>
   )
 }
