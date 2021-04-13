6e487cec9a5a755a624140dbfa281fb5f1117c41
CD-Mackay <connor.mackay@gmail.com>
2021-04-13 14:10:43 -0400

    add generate fibonacci sequence to listitem components

--- src/Components/SequenceListItems.jsx
+++ src/Components/SequenceListItems.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ListItem(props) {
 
   const view = () => {
-   console.log(props.onSelect(props.numone, props.numTwo));
+   console.log(props.onSelect(props.numone, props.numTwo, props.numThree));
   }
   return (
    <button onClick={view}>{props.numone}{props.numTwo}</button>
