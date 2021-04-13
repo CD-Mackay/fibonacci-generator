c4ccbb7111839ab0b032b9680c57a13d39af5486
CD-Mackay <connor.mackay@gmail.com>
2021-04-13 11:29:40 -0400

    update edit buttons

--- src/Components/SequenceList.jsx
+++ src/Components/SequenceList.jsx
@@ -4,7 +4,7 @@ import ListItem from './SequenceListItems';
 export default function SequenceList(props) {
 
   const parsedSequences = props.sequences.map(seq => {
-    return <ListItem id={seq.id} key={seq.id} numone={seq.num_one} numTwo={seq.num_two} />
+    return <ListItem id={seq.id} key={seq.id} numone={seq.num_one} numTwo={seq.num_two} onSelect={props.onView} />
   })
   return (
     <div>
