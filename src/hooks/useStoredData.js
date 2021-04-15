29c716c2b681ff87356f5cd994df63710d3d2bb1
CD-Mackay <connor.mackay@gmail.com>
2021-04-15 17:38:19 -0400

    implement validation function

--- src/hooks/useStoredData.js
+++ src/hooks/useStoredData.js
@@ -20,6 +20,19 @@ const useStoredData = () => {
     .catch(err => console.log(err));
   }, []);
 
+  function sequenceExists(obj) {
+    for (const element in state) {
+      let object = state[element]
+      delete object.id;
+      if (_.isEqual(obj, object)) {
+        console.log('sequence exists');
+        return true;
+      }
+    }; 
+    console.log('sequence does not exist');
+    return false;
+  }
+
   function saveSequence(num1, num2, num3) {
     const sequence = {
       num_one: num1,
@@ -27,18 +40,16 @@ const useStoredData = () => {
       num_three: num3
     };
 
-    state.forEach(element => {
-      if (_.isEqual(sequence, element)) {
-        console.log('sequence exists');
-      }
-    })
-
+    if (!sequenceExists(sequence)) {
     return axios({
       method: 'post',
       url: '/sequences',
       data:  sequence 
     })
     .catch(err => console.log(err));
+  } else {
+    console.log('save failed');
+  }
   };
 
   return {
