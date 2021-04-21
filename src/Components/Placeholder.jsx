6ffd30ee3f02fac1df7e0da59c8e6f19a8ded184
CD-Mackay <connor.mackay@gmail.com>
2021-04-21 14:58:46 -0400

    style placeholder component

--- src/Components/Placeholder.jsx
+++ src/Components/Placeholder.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import fibonacci from '../images/fibonacci.jpeg';
+import '../styles.scss';
 
 export default function Placeholder() {
   return (
@@ -8,7 +9,10 @@ export default function Placeholder() {
       <p>I have always been intrigued by Fibonacci sequences and their appearance in nature.</p>
       <p>This project was first inspired by a challenge to produce an algorithm which returned a Fibonacci sequence.</p>
       <p>Add React, Express and Postgresql to create the Fibonacci Generator!</p>
-      <img src={fibonacci} alt="fibonacci" ></img>
+      <div className="image-wrapper">
+      <img src={fibonacci} alt="fibonacci" />
+      <p>The Golden Ratio in nature</p>
+      </div>
     </div>
   )
 }
