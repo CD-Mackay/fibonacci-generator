d99a37b823bf01cad8208e5ee49ac258b7479a2a
CD-Mackay <connor.mackay@gmail.com>
2021-04-21 14:49:19 -0400

    add content to placeholder

--- src/Components/Placeholder.jsx
+++ src/Components/Placeholder.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 
 export default function Placeholder() {
   return (
-    <div>I am placeholder</div>
+    <div>
+      <h4>Welcome to the Fibonacci Generator</h4>
+      <p>I have always been intrigued by Fibonacci sequences and their appearance in nature.</p>
+      <p>This project was first inspired by a challenge to produce an algorithm which returned a Fibonacci sequence.</p>
+      <p>Add React, Express and Postgresql to create the Fibonacci Generator!</p>
+    </div>
   )
 }
