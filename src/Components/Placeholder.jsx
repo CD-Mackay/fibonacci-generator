4eaf105efd3b33824df6fefb3ddc3bb473072d1e
CD-Mackay <connor.mackay@gmail.com>
2021-04-21 14:54:32 -0400

    add image to placeholder

--- src/Components/Placeholder.jsx
+++ src/Components/Placeholder.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import fibonacci from '../images/fibonacci.jpeg';
 
 export default function Placeholder() {
   return (
-    <div>
+    <div className="placeholder">
       <h4>Welcome to the Fibonacci Generator</h4>
       <p>I have always been intrigued by Fibonacci sequences and their appearance in nature.</p>
       <p>This project was first inspired by a challenge to produce an algorithm which returned a Fibonacci sequence.</p>
       <p>Add React, Express and Postgresql to create the Fibonacci Generator!</p>
+      <img src={fibonacci} alt="fibonacci" ></img>
     </div>
   )
 }
